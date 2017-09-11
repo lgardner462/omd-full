768bacec33d5e682485000d1f6fee22972bd54cd
OMD site neuro <neuro@om-noc.cm.cluster>
2017-09-11 14:59:48 -0400

    -a

--- groups.mk
+++ groups.mk
@@ -3,21 +3,21 @@
 
 if type(define_hostgroups) != dict:
     define_hostgroups = {}
-define_hostgroups.update({'neuro-basic': u'neuro-basic',
- 'neuro-critical': u'neuro-critical',
- 'neuro-critical-24x7': u'neuro-critical-24x7',
+define_hostgroups.update({'tsstuff-basic': u'tsstuff-basic',
+ 'tsstuff-critical': u'tsstuff-critical',
+ 'tsstuff-critical-24x7': u'tsstuff-critical-24x7',
  'testhostgroup': u'testhostgroup'})
 
 if type(define_servicegroups) != dict:
     define_servicegroups = {}
-define_servicegroups.update({'neuro-basic': u'neuro-basic',
- 'neuro-critical': u'neuro-critical',
- 'neuro-critical-24x7': u'neuro-critical-24x7',
+define_servicegroups.update({'tsstuff-basic': u'tsstuff-basic',
+ 'tsstuff-critical': u'tsstuff-critical',
+ 'tsstuff-critical-24x7': u'tsstuff-critical-24x7',
  'public': u'public'})
 
 if type(define_contactgroups) != dict:
     define_contactgroups = {}
-define_contactgroups.update({'neuro-admins': u'neuro-admins',
- 'neuro-pager': u'neuro-pager',
- 'neuro-ts': u'neurots'})
+define_contactgroups.update({'tsstuff-admins': u'tsstuff-admins',
+ 'tsstuff-pager': u'tsstuff-pager',
+ 'tsstuff-ts': u'tsstuffts'})
 
--- printer.mk
+++ printer.mk
@@ -1,5 +1,5 @@
 host_groups = [
-  ( 'neuro-printer', ['neuro-printer'], ALL_HOSTS ),   
+  ( 'tsstuff-printer', ['tsstuff-printer'], ALL_HOSTS ),   
   ( 'snmp', ['snmp'], ALL_HOSTS ), 
 ] + host_groups
 
@@ -7,69 +7,69 @@ host_groups = [
 extra_service_conf.setdefault('_ec_sl', [])
 
 extra_service_conf['_ec_sl'] = [
-  ( 0, ['neuro-printer'] , ALL_HOSTS, ALL_SERVICES),
+  ( 0, ['tsstuff-printer'] , ALL_HOSTS, ALL_SERVICES),
 ] + extra_service_conf['_ec_sl']
 
 
 extra_host_conf.setdefault('check_interval', [])
 
 extra_host_conf['check_interval'] = [
-  ( 5, ['neuro-printer', ], ALL_HOSTS ),
+  ( 5, ['tsstuff-printer', ], ALL_HOSTS ),
 ] + extra_host_conf['check_interval']
 
 
 host_contactgroups = [
-  ( 'neuro-admins', ['neuro-printer'], ALL_HOSTS),
+  ( 'tsstuff-admins', ['tsstuff-printer'], ALL_HOSTS),
 ] + host_contactgroups
 
 
 extra_service_conf.setdefault('check_interval', [])
 
 extra_service_conf['check_interval'] = [
-  ( 5, ['neuro-printer', ], ALL_HOSTS, ALL_SERVICES ),
+  ( 5, ['tsstuff-printer', ], ALL_HOSTS, ALL_SERVICES ),
 ] + extra_service_conf['check_interval']
 
 
 extra_host_conf.setdefault('notification_interval', [])
 
 extra_host_conf['notification_interval'] = [
-  ( 60, ['neuro-printer', ], ALL_HOSTS ),
+  ( 60, ['tsstuff-printer', ], ALL_HOSTS ),
 ] + extra_host_conf['notification_interval']
 
 
 extra_service_conf.setdefault('notification_period', [])
 
 extra_service_conf['notification_period'] = [
-  ( 'allday', ['neuro-printer', ], ALL_HOSTS, ALL_SERVICES ),
+  ( 'allday', ['tsstuff-printer', ], ALL_HOSTS, ALL_SERVICES ),
 ] + extra_service_conf['notification_period']
 
 
 extra_service_conf.setdefault('notification_interval', [])
 
 extra_service_conf['notification_interval'] = [
-  ( 60, ['neuro-printer', ], ALL_HOSTS, ALL_SERVICES ),
+  ( 60, ['tsstuff-printer', ], ALL_HOSTS, ALL_SERVICES ),
 ] + extra_service_conf['notification_interval']
 
 
 service_contactgroups = [
-  ( 'neuro-admins', ['neuro-printer', ], ALL_HOSTS, ALL_SERVICES ),
+  ( 'tsstuff-admins', ['tsstuff-printer', ], ALL_HOSTS, ALL_SERVICES ),
 ] + service_contactgroups
 
 
 service_groups = [
-  ( 'neuro-printer', ['neuro-printer', ], ALL_HOSTS, ALL_SERVICES ),
+  ( 'tsstuff-printer', ['tsstuff-printer', ], ALL_HOSTS, ALL_SERVICES ),
 ] + service_groups
 
 
 host_groups = [
-  ( 'neuro-printer', ['neuro-printer', ], ALL_HOSTS ),
+  ( 'tsstuff-printer', ['tsstuff-printer', ], ALL_HOSTS ),
 ] + host_groups
 
 
 extra_host_conf.setdefault('notification_period', [])
 
 extra_host_conf['notification_period'] = [
-  ( 'allday', ['neuro-printer', ], ALL_HOSTS ),
+  ( 'allday', ['tsstuff-printer', ], ALL_HOSTS ),
 ] + extra_host_conf['notification_period']
 
 
@@ -87,181 +87,181 @@ notification_rules += [{'allow_disable': True,
   'comment': u'printer host first two emails (ack) \n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'print host first 2 emails, one per hour (ack)',
   'disabled': False,
   'match_escalation': (1, 2),
   'match_host_event': ['x'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n',
-                     'host_subject': u'neuro ACK alert: $HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff ACK alert: $HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host first two emails (sched downtime) \n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'printer host first 2 emails, one per hour (sched)',
   'disabled': False,
   'match_escalation': (1, 2),
   'match_host_event': ['s'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n',
-                     'host_subject': u'neuro scheduled downtime: $HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff scheduled downtime: $HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host first two emails (flapping) \n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'printer host first 2 emails, one per hour (flap)',
   'disabled': False,
   'match_escalation': (1, 2),
   'match_host_event': ['f'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n',
-                     'host_subject': u'neuro FLAPPING alert: $HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff FLAPPING alert: $HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host first two emails (recovery) \n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'printer host first 2 emails, one per hour (recov)',
   'disabled': False,
   'match_escalation': (1, 2),
   'match_host_event': ['dr', 'ur'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n',
-                     'host_subject': u'neuro RECOVERY alert: $HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff RECOVERY alert: $HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host first two emails (problem) \n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'printer host first 2 emails, one per hour (problem)',
   'disabled': False,
   'match_escalation': (1, 2),
   'match_host_event': ['rd', 'ru', 'du', 'ud'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n',
-                     'host_subject': u'neuro PROBLEM alert: $HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff PROBLEM alert: $HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host escalation emails,send daily (ack)\n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'host printer escalation emails, send daily  (ack)',
   'disabled': False,
   'match_escalation': (3, 999999),
   'match_host_event': ['x'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n\n',
-                     'host_subject': u'neuro ACK alert:$HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff ACK alert:$HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host escalation emails,send daily (sched downtime)\n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'host printer escalation emails, send daily (sched)',
   'disabled': False,
   'match_escalation': (3, 999999),
   'match_host_event': ['s'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n\n',
-                     'host_subject': u'neuro scheduled downtime:$HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff scheduled downtime:$HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host escalation emails,send daily (flap) \n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'host printer escalation emails, send daily  (flap)',
   'disabled': False,
   'match_escalation': (3, 999999),
   'match_host_event': ['f'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n\n',
-                     'host_subject': u'neuro FLAPPING alert:$HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff FLAPPING alert:$HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host escalation emails,send daily (problem)\n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'host printer escalation emails, send daily (prob)',
   'disabled': False,
   'match_escalation': (3, 999999),
   'match_host_event': ['rd', 'ru', 'du', 'ud'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n\n',
-                     'host_subject': u'neuro PROBLEM alert:$HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff PROBLEM alert:$HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
  {'allow_disable': True,
   'comment': u'printer host escalation emails,send daily (recovery) \n',
   'contact_all': False,
   'contact_all_with_email': False,
-  'contact_groups': ['neuro-admins'],
+  'contact_groups': ['tsstuff-admins'],
   'contact_object': False,
   'description': u'host printer escalation emails, send daily  (recovery)',
   'disabled': False,
   'match_escalation': (3, 999999),
   'match_host_event': ['dr', 'ur'],
-  'match_hostgroups': ['neuro-printer'],
+  'match_hostgroups': ['tsstuff-printer'],
   'notify_plugin': ('asciimail',
                     {'common_body': u'Host:     $HOSTNAME$\n',
-                     'from': 'neuro-noc@techsquare.com',
+                     'from': 'tsstuff-noc@techsquare.com',
                      'host_body': u'Event:    $EVENT_TXT$\nOutput:   $HOSTOUTPUT$\nPerfdata: $HOSTPERFDATA$\n$LONGHOSTOUTPUT$\n\n',
-                     'host_subject': u'neuro RECOVERY alert:$HOSTNAME$ - $EVENT_TXT$',
-                     'reply_to': 'neuro-noc@techsquare.com',
+                     'host_subject': u'tsstuff RECOVERY alert:$HOSTNAME$ - $EVENT_TXT$',
+                     'reply_to': 'tsstuff-noc@techsquare.com',
                      'service_body': u'Service:  $SERVICEDESC$\nEvent:    $EVENT_TXT$\nOutput:   $SERVICEOUTPUT$\nPerfdata: $SERVICEPERFDATA$\n$LONGSERVICEOUTPUT$\n'})},
 
 ]
